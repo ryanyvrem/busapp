d796759b42b76744c8c1c11dc4fc3108fc53f4ab
Mervyn <limryan@sas.upenn.edu>
2020-12-14 10:58:07 +0800

    first

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <View style={styles.container}>
+     <Text style={styles.title}>Bus arriving in</Text>
+     <Text style={styles.arrivalTime}>Loading...</Text>
+     <TouchableOpacity style={styles.button}>
+       <Text style={styles.buttonText}>Refresh!</Text>
+     </TouchableOpacity>
+   </View>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+   justifyContent: "center",
+ },
+ title: {
+   fontWeight: "bold",
+   fontSize: 32,
+   marginBottom: 24,
+ },
+ arrivalTime: {
+   fontSize: 64,
+   marginBottom: 32,
+ },
+ button: {
+   padding: 20,
+   backgroundColor: "red",
+ },
+ buttonText: {
+   fontSize: 32,
+   fontWeight: "bold",
+   color: "white",
+ },
 });
+
